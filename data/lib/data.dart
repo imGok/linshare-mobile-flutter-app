6af5dc7d61c459c82a98b69a4f9580d00641124b
Julian KOUNE <jkoune@linagora.com>
2021-03-25 19:36:04 +0100

    LA-247 Get Shared Space Members Data Layer

--- data/lib/data.dart
+++ data/lib/data.dart
@@ -11,6 +11,7 @@ export 'src/datasource/shared_space_document_datasource.dart';
 export 'src/datasource/quota_datasource.dart';
 export 'src/datasource/functionality_datasource.dart';
 export 'src/datasource/sort_datasource.dart';
+export 'src/datasource/shared_space_member_datasource.dart';
 
 // data source impl
 export 'src/datasource_impl/document_datasource_impl.dart';
@@ -22,6 +23,7 @@ export 'src/datasource_impl/autocomplete_datasource_impl.dart';
 export 'src/datasource_impl/quota_datasource_impl.dart';
 export 'src/datasource_impl/functionality_datasource_impl.dart';
 export 'src/datasource_impl/sort_datasource_impl.dart';
+export 'src/datasource_impl/shared_space_member_datasource_impl.dart';
 
 // config
 export 'src/network/config/dynamic_url_interceptors.dart';
@@ -46,6 +48,8 @@ export 'src/network/model/autocomplete/mailing_list_autocomplete_result_dto.dart
 export 'src/network/model/autocomplete/simple_autocomplete_result_dto.dart';
 export 'src/network/model/autocomplete/user_autocomplete_result_dto.dart';
 export 'src/network/model/request/create_shared_space_node_folder_request.dart';
+export 'package:data/src/network/model/response/shared_space_member_response.dart';
+export 'package:data/src/network/model/response/shared_space_member_node_dto.dart';
 
 // repository
 export 'src/repository/authentication/authentication_repository_impl.dart';
@@ -59,6 +63,7 @@ export 'src/repository/sharedspacedocument/shared_space_document_repository_impl
 export 'src/repository/quota/quota_repository_impl.dart';
 export 'src/repository/functionality/functionality_repository_impl.dart';
 export 'src/repository/sort/sort_repository_impl.dart';
+export 'src/repository/sharedspacemember/shared_space_member_repository_impl.dart';
 
 // util
 export 'src/util/device_manager.dart';
